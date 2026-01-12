f9fbdeacba2203edc106ac78c34e3c7524c5d3f9
RiKy-07 <ramosruikiamy2121@gmail.com>
2026-01-12 16:49:16 +0100

    Update: Added Project section

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -48,6 +48,80 @@ skillsHeader.forEach((el) => {
     el.addEventListener('click', toggleSkills)
 })
 // QUALIFICATION TABS
+const tabs = document.querySelectorAll('[data-target]')
+      tabContents = document.querySelectorAll('[data-content]');
+
+      tabs.forEach(tab => {
+        tab.addEventListener('click', () => {
+            const target = document.querySelector(tab.dataset.target);
+
+            tabContents.forEach(tabContent => {
+                tabContent.classList.remove('qualification__active')
+            })
+            target.classList.add('qualification__active');
+
+            tabs.forEach(tab => {
+                tab.classList.remove('qualification__active');
+            })
+            tab.classList.add('qualification__active');
+        })
+      })
 
 // SERVICES MODAL
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('.services__button'),
+      modalCloses = document.querySelectorAll('.services__modal-close');
 
+let modal = function(modalClick) {
+    modalViews[modalClick].classList.add('active-modal');
+}
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i);
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal');
+        })
+    })
+})
+
+// PORTFOLIO SWIPER
+
+let swiperPortfolio = new Swiper('.portfolio__container', {
+    cssMode: true,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    // mousewheel: true,
+    // keyboard: true,
+}) 
+
+// TESTIMONIAL SWIPER
+
+let swiperTestimonials = new Swiper('.testimonial__container', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true
+    },
+    // mousewheel: true,
+    // keyboard: true,
+}) 
